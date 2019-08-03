99d95b8a32f4fc6c8be7c5157909bb2d70654cc9
lr <lr@rui.com>
2019-08-03 20:26:45 +0800

    添加print

--- qingcheng_service_goods/src/main/java/com/qingcheng/consumer/BackMessageConsumer.java
+++ qingcheng_service_goods/src/main/java/com/qingcheng/consumer/BackMessageConsumer.java
@@ -23,6 +23,7 @@ public class BackMessageConsumer implements MessageListener {
         } catch (Exception e) {
             e.printStackTrace();
             // 记录日志人工干预
+            System.out.println("日志");
         }
     }
 }
